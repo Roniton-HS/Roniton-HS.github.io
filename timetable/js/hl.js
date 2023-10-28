d1ae0dfaa7c09627f3289fa8cb62bb67c9283b29
Henrik Starke <52321099+Roniton-HS@users.noreply.github.com>
2023-10-28 04:58:22 +0200

    Add files via upload

--- timetable/js/hl.js
+++ timetable/js/hl.js
@@ -4,11 +4,16 @@ const inRange = function(i, low, high) {
 
 const getTableCell = function(t, day) {
   let cell;
-  if      (inRange(t, 800, 959)) cell = 1
-  else if (inRange(t, 1000, 1159)) cell = 2
-  else if (inRange(t, 1200, 1359)) cell = 3
-  else if (inRange(t, 1400, 1559)) cell = 4
-  else if (inRange(t, 1600, 1759)) cell = 5;
+  if      (inRange(t, 800, 859)) cell = 1
+  else if (inRange(t, 900, 959)) cell = 2
+  else if (inRange(t, 1000, 1059)) cell = 3
+  else if (inRange(t, 1100, 1159)) cell = 4
+  else if (inRange(t, 1200, 1259)) cell = 5
+  else if (inRange(t, 1300, 1359)) cell = 6
+  else if (inRange(t, 1400, 1459)) cell = 7
+  else if (inRange(t, 1500, 1559)) cell = 8
+  else if (inRange(t, 1600, 1659)) cell = 9
+  else if (inRange(t, 1700, 1759)) cell = 10;
   return `${day}${cell}`;
 }
 
